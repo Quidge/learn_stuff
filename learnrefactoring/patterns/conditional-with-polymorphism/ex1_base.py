43e7a624f6e94a9520956c9ba89a5a7f348360c2
Jonathan Demirgian <jd@Jonathans-MBP.attlocal.net>
2020-01-04 16:17:39 -0500

    refactor variant logic off of superclass and into subclasses

--- learnrefactoring/patterns/conditional-with-polymorphism/ex1_base.py
+++ learnrefactoring/patterns/conditional-with-polymorphism/ex1_base.py
@@ -26,40 +26,45 @@ def createBird(**kwargs):
     else:
         return Bird(**kwargs)
 
+
 class Bird:
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
 
     @property
     def plumage(self):
-        if self.type == 'EuropeanSwallow':
-            return 'average'
-        elif self.type == 'AfricanSwallow':
-            return 'tired' if self.numberOfCoconuts > 2 else 'average'
-        elif self.type == 'NorwegianBlueParrot':
-            return 'scorched' if self.voltage > 100 else 'beautiful'
-        else:
-            return 'unknown'
+        return 'unknown'
 
     @property
     def air_speed_velocity(self):
-        if self.type == 'EuropeanSwallow':
-            return 35
-        elif self.type == 'AfricanSwallow':
-            return 40 - 2 * self.numberOfCoconuts
-        elif self.type == 'NorwegianBlueParrot':
-            return 0 if self.isNailed else 10 + int(self.voltage / 10)
-        else:
-            return None
+        return None
 
 
 class EuropeanSwallow(Bird):
-    pass
+    @property
+    def plumage(self):
+        return 'average'
+
+    @property
+    def air_speed_velocity(self):
+        return 35
 
 
 class AfricanSwallow(Bird):
-    pass
+    @property
+    def plumage(self):
+        return 'tired' if self.numberOfCoconuts > 2 else 'average'
+
+    @property
+    def air_speed_velocity(self):
+        return 40 - 2 * self.numberOfCoconuts
 
 
 class NorwegianBlueParrot(Bird):
-    pass
+    @property
+    def plumage(self):
+        return 'scorched' if self.voltage > 100 else 'beautiful'
+
+    @property
+    def air_speed_velocity(self):
+        return 0 if self.isNailed else 10 + int(self.voltage / 10)
